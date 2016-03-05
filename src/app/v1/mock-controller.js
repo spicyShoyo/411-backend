01f43ce4469469e4aab962945f82907fed43b49d
lafickens <yuxiaohang87878@msn.com>
2016-03-05 15:18:48 -0600

    Mock controller

--- src/app/v1/mock-controller.js
+++ src/app/v1/mock-controller.js
@@ -3,13 +3,20 @@
  */
 
 const Controller = require('./../controller');
-const { GET } = require('./../http-methods');
+const { GET, POST } = require('./../http-methods');
+const UseMiddleware = require('./../use-middleware');
+const requireParams = require('./../middleware/require-params');
 
 export default class MockController extends Controller {
 
   get prefix() { return 'mock'; }
 
-  @GET('')
+  @POST('')
+  @UseMiddleware([
+    requireParams({
+      a: String
+                  })
+  ])
   mock(req, res, next) {
     res.send({a: 'b'});
     return next();
