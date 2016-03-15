0e98038fd136ff59596a5590b9f38e61d542d879
spicyShoyo <lzhengchao18@outlook.com>
2016-03-14 21:25:58 -0500

    dup like

--- src/app/v1/like-controller.js
+++ src/app/v1/like-controller.js
@@ -3,7 +3,9 @@
  */
 
 const Controller = require('./../controller');
-const { POST } = require('./../http-methods');
+const {
+	POST
+} = require('./../http-methods');
 const UseMiddleware = require('./../use-middleware');
 const requireParams = require('./../middleware/require-params');
 const db = require('./../database');
@@ -11,30 +13,42 @@ const Errors = require('restify-errors');
 
 export default class likeController extends Controller {
 
-  get prefix() { return 'like'; }
+	get prefix() {
+		return 'like';
+	}
 
-  /**
-   * @api {get} /v1/like
-   * @apiName like
-   * @apiGroup like
-   *
-   * @apiParam username, string
-   *            drinkname, string
-   * @apiSuccess {String} drinks row returned
-   *
-   */
-  @POST('')
-  like(req, res, next) {
-    let drinkname = req.body.drinkname;
-    let username = req.body.username;
-    let connection;
-    db.then(conn => {
-        connection = conn;
-        return conn.query(`UPDATE drink SET likes=likes+1 WHERE drinkname='${drinkname}';`);
-      }).then(rows=> {
-         connection.query(`INSERT INTO likedrink (username, drinkname) VALUES ('${username}', '${drinkname}')`);
-        res.send({drinks:rows});
-        return next();
+	/**
+	 * @api {get} /v1/like
+	 * @apiName like
+	 * @apiGroup like
+	 *
+	 * @apiParam username, string
+	 *            drinkname, string
+	 * @apiSuccess {String} drinks row returned
+	 *
+	 */
+	@POST('')
+	like(req, res, next) {
+		let drinkname = req.body.drinkname;
+		let username = req.body.username;
+		let connection;
+		db.then(conn => {
+			connection = conn;
+			return conn.query(`UPDATE drink SET likes=likes+1 WHERE drinkname='${drinkname}';`);
+		}).then(rows => {
+			return connection.query(`INSERT INTO likedrink (username, drinkname) VALUES ('${username}', '${drinkname}')`);
+		}).then(rows => {
+      let retStr=`Awesome! You just liked ${drinkname}!`;
+      res.send({
+        drinks: retStr
       });
-  }
+      return next();
+		}).catch(err=> {
+      let retStr=`You have already liked ${drinkname}!`;
+      res.send({
+        drinks: retStr
+      });
+      return next();
+    });
+	}
 }
