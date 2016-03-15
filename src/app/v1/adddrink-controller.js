f4b2763e2674cf47849a3e773b9e871fad9668ff
mliu-dark-knight <mengxiongliu@wirelessprvnat-172-17-46-8.near.illinois.edu>
2016-03-14 20:10:45 -0500

    add drink

---
+++ src/app/v1/adddrink-controller.js
@@ -0,0 +1,44 @@
+/**
+ * Created by @tourbillon on 3/8/16.
+ */
+
+const Controller = require('./../controller');
+const { POST } = require('./../http-methods');
+const UseMiddleware = require('./../use-middleware');
+const requireParams = require('./../middleware/require-params');
+const db = require('./../database');
+const Errors = require('restify-errors');
+
+export default class addDrinkController extends Controller {
+
+  get prefix() { return 'adddrink'; }
+
+  /**
+   * @api {get} /v1/like
+   * @apiName like
+   * @apiGroup like
+   *
+   * @apiParam username, string
+   *            drinkname, string
+   * @apiSuccess {String} drinks row returned
+   *
+   */
+  @POST('')
+  adddrink(req, res, next) {
+    let drinkname = req.body.drinkname;
+    let category = req.body.category;
+    let alcohol = "Alcoholic";
+    let glass = req.body.glass;
+    let likes = 0;
+    let ingredientname = req.body.ingredientname;
+    let connection;
+    db.then(conn => {
+        connection = conn;
+        let url = connection.query(`SELECT u FROM pic ORDER BY RANDOM() LIMIT 1`);
+        connection.query(`INSERT INTO ingredientof (drinkname, ingredientname) VALUES ('${drinkname}', '${ingredientname}')`);
+        connection.query(`INSERT INTO drink (drinkname, category, alcohol, glass, likes, url)
+                          VALUES ('${drinkname}', '${category}', '${alcohol}', '${glass}', '${likes}', '${url}')`);
+        return next();
+      });
+  }
+}
