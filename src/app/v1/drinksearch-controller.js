7d9b020a3c4808071537e248b49529500731e5c9
spicyShoyo <lzhengchao18@outlook.com>
2016-03-15 22:45:04 -0500

    drink search revised

--- src/app/v1/drinksearch-controller.js
+++ src/app/v1/drinksearch-controller.js
@@ -3,7 +3,9 @@
  */
 
 const Controller = require('./../controller');
-const { POST } = require('./../http-methods');
+const {
+	POST
+} = require('./../http-methods');
 const UseMiddleware = require('./../use-middleware');
 const requireParams = require('./../middleware/require-params');
 const db = require('./../database');
@@ -11,34 +13,59 @@ const Errors = require('restify-errors');
 
 export default class drinksearchController extends Controller {
 
-  get prefix() { return 'drinksearch'; }
+	get prefix() {
+		return 'drinksearch';
+	}
 
-  /**
-   * @api {post} /v1/drinksearch Drinksearch
-   * @apiName Drinksearch
-   * @apiGroup Drinksearch
-   *
-   * @apiParam typed drink name typed so far.
-   *
-   * @apiSuccess {String} drinks drinks with name contains the drinktyped.
-   *
-   */
-  @UseMiddleware([
-    requireParams({
-      drinktyped: String,
-                  })
-                 ])
+	/**
+	 * @api {post} /v1/drinksearch Drinksearch
+	 * @apiName Drinksearch
+	 * @apiGroup Drinksearch
+	 *
+	 * @apiParam typed drink name typed so far.
+	 *
+	 * @apiSuccess {String} drinks drinks with name contains the drinktyped.
+	 *
+	 */
+	@UseMiddleware([
+		requireParams({
+			param: String,
+			searchby: String,
+		})
+	])
 
-  @POST('')
-  drinksearch(req, res, next) {
-    let drinktyped = req.body.drinktyped;
-    let connection;
-    db.then(conn => {
-        connection = conn;
-        return conn.query(`SELECT * FROM drink WHERE drinkname LIKE '%${drinktyped}%' LIMIT 5;`);
-      }).then(rows=> {
-        res.send({drinks:rows});
-        return next();
-      });
-  }
+	@POST('')
+	drinksearch(req, res, next) {
+		let param = req.body.param;
+		let searchby = req.body.searchby;
+		let connection;
+		if (searchby == 'drinkname') {
+			db.then(conn => {
+				connection = conn;
+				return conn.query(`SELECT * FROM drink WHERE drinkname LIKE '%${param}%' LIMIT 8;`);
+			}).then(rows => {
+				for (let i = 0; i < 8; ++i) {
+					if (i < rows.length) {
+						rows[i]["featured"] = false;
+					}
+				}
+				if (0 < rows.length) {
+					rows[0]["featured"] = true;
+				}
+				if (3 < rows.length) {
+					rows[3]["featured"] = true;
+				}
+				res.send({
+					drinks: rows
+				});
+				return next();
+			});
+		} else {
+			//to be added
+			res.send({
+				drinks: []
+			});
+			return next();
+		}
+	}
 }
